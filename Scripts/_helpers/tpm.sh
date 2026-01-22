b3a93f83a8a4b7a97312c996763887d7221e0652
0LA-X <165085183+0LA-X@users.noreply.github.com>
2026-01-22 07:43:58 +0100

    Updated - logic & added install func

--- Scripts/_helpers/tpm.sh
+++ Scripts/_helpers/tpm.sh
@@ -13,18 +13,27 @@ install_tpm() {
   else
     echo -e "TPM already installed at $TPM_DIR."
   fi
+}
 
+install_plug() {
   echo -e "[ + ] Installing TPM plugins..."
   "$TPM_DIR/bin/install_plugins"
 }
 
-# Check if tmux.conf exists & source it.
+# -- Check if tmux.conf exists & source it.
 config_tmux(){
   if [[ -f "$TMUX_CONF" ]]; then
     echo -e "[ ! ] Detected tmux.conf at $TMUX_CONF"
+    
+    # Install TPM BEFORE sourcing config
+    install_tpm
+    
+    # Source the config
     tmux source "$TMUX_CONF"
     echo -e "[ ! ] Sourced tmux config ....."
-    install_tpm
+    
+    # Install plugins AFTER sourcing
+    install_plug
   else
     echo -e "[ ? ] No tmux.conf found in ~/.config/tmux ."
   fi
